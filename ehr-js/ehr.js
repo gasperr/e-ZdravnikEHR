778e4c9ef61ec1a245670cb10143864288ee8b42
gasperr <gasper.andrejc@gmail.com>
2015-02-02 13:59:34 +0100

    Update ehr.js

--- ehr-js/ehr.js
+++ ehr-js/ehr.js
@@ -9,8 +9,8 @@ var password = "ois4fri";
 
 
 var ehrIdDemolis = "a74c3564-a6ee-4796-a5de-914d7fe9332e";
-var ehrIdKonda = "67dad48d-42ca-4d61-9ab6-bbad1b990168";
-var ehrIdNerovac = "cc62a1a5-14fc-44e7-9837-e9199e248cf6";
+var ehrIdKonda = "6e9c47e2-bdc0-40fe-b2de-c17dba7366c7";
+var ehrIdNerovac = "9ca04654-c3dd-4c8d-bf7b-36d35df4b034";
 
 function getSessionId() {
     var response = $.ajax({
